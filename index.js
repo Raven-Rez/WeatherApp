71da465a04a1efe77396ad48f90db7c4146498ad
Raven-Rez <misery1290@gmail.com>
2024-06-25 00:30:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const btnEl = document.getElementById("search-btn");
 const searchEl = document.getElementById("input-box");
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 const apiKey = "8b1e7daaa6f20479706438d703cb224e" // &appid=;
+const temperature = document.querySelector(".temperature");
+const weatherIconEl = document.querySelector(".weather-icon");
+const humidity = document.querySelector(".humidity");
+const windSpeed = document.querySelector(".wind-speed");
 let cityName = "hi";
 btnEl.addEventListener("click", function(){
     cityName = searchEl.value;
@@ -19,6 +23,10 @@ function checkWeather(){
     .then((res) => res.json())
     .then((data) => {
         displayCityName.textContent = data.name;
+        temperature.textContent = `${parseInt(data.main.temp)}°C`;
+        humidity.textContent = `${data.main.humidity}%`;
+        windSpeed.textContent = `${parseInt(data.wind.speed)} km/h`;
+        weatherIconEl.src = `images/${data.weather[0].main.toLowerCase()}.png`;
     });
     
 }
